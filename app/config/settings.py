3cdb89e5b0ee90555f2c6434ef84d7b9cf528da8
САНЧЕЗ <katecat1010@gmail.ru>
2026-02-09 15:51:42 +0300

    переделала класс DatabaseSettings

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,3 +1,6 @@
+# from functools import cached_property
+
+import os
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -6,21 +9,21 @@ from pydantic_settings import (
 
 class DatabaseSettings(BaseSettings):
 
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="DB_",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
-    DB_ECHO: bool
 
-    @property
-    def DB_URL(self) -> str:
+    def get_dsn(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_prefix="DB_",
-        case_sensitive=False
-    )
-
-settings = DatabaseSettings
+settings = DatabaseSettings()
--- app/db_connection.py
+++ app/db_connection.py
@@ -10,8 +10,8 @@ from app.config.settings import settings
 
 
 db_engine = create_async_engine(
-    url=settings.DB_URL, #type: ignore
-    echo=settings.DB_ECHO,
+    url=settings.get_dsn(), #type: ignore
+    echo=True,
 )
 
 Async_session_local = async_sessionmaker(
